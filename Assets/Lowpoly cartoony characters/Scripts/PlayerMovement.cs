237de76ff12871a659bec0f29cddbea362b2ffd6
42Nikita <nikita.ovdiienko@gmail.com>
2025-05-25 17:53:13 +0300

    add jump movement

---
+++ Assets/Lowpoly cartoony characters/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private float jumpForce = 15f;
+    private float gravity = 5f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        } if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+            isGrounded = true;
+        }
+       
+       
+        
+    }
+}
